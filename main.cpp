300f142cbe98f5e35f3647dc7fe4d7f4685459c0
Sofia Michailidu <sofia@michailidu.cz>
2023-10-29 09:03:05 +0100

    fix

--- main.cpp
+++ main.cpp
@@ -17,7 +17,7 @@ vector<int> best_permutation = std::vector<int>();
 int best_distance = INT_MAX;
 std::mutex m;
 
-vector<int> ithPermutation(int i)
+vector<int> findNthPermutation(int n)
 {
 	int j, k = 0;
 	vector<int> fact = vector<int>();
@@ -29,8 +29,8 @@ vector<int> ithPermutation(int i)
 
 	for (k = 0; k < number_of_items; ++k)
 	{
-		perm.push_back(i / fact.at(number_of_items - 1 - k));
-		i = i % fact.at(number_of_items - 1 - k);
+		perm.push_back(n / fact.at(number_of_items - 1 - k));
+		n = n % fact.at(number_of_items - 1 - k);
 	}
 
 	for (k = number_of_items - 1; k > 0; --k)
@@ -39,6 +39,9 @@ vector<int> ithPermutation(int i)
 				perm.at(k)++;
 
 	return perm;
+}
+int factorial(int n) {
+	return (n == 1 || n == 0) ? 1 : factorial(n - 1) * n;
 }
 
 void readMatricesFromFile(const std::string& filename) {
@@ -93,33 +96,31 @@ int get_permutation_distance(vector<int> permutation) {
 	int distance = 0;
 	for (int i = 0; i < number_of_items; i++)
 	{
-		for (int j = i; j < number_of_items; j++)
+		for (int j = 0; j < number_of_items; j++)
 		{
-			distance += distances[permutation.at(i)][permutation.at(j)] * flows[i][j] * 2;
+			distance += distances[permutation.at(i)][permutation.at(j)] * flows[i][j];
 			if (distance > best_distance)
-				return -i;
+				return -(i % number_of_items);
 		}
 
 	}
 	return distance;
 }
-int factorial(int n) {
-	return (n == 1 || n == 0) ? 1 : factorial(n - 1) * n;
-}
 
 void get_permutations_distances(int i_from, int i_to)
 {
-	vector<int> permutation = ithPermutation(i_from);
-
-	for (int i = i_from; i < i_to; i++)
+	vector<int> permutation = findNthPermutation(i_from);
+	int i = 0;
+	while (i <= i_to - i_from)
 	{
 		int d = get_permutation_distance(permutation);
-		if (d < 0)
+		if (d <= 0)
 		{
 			int prev = permutation[-d];
-			while (i < i_to && permutation[-d] == prev) {
-				std::next_permutation(permutation.begin() + 1, permutation.end());
+			while (std::next_permutation(permutation.begin(), permutation.end()))
+			{
 				i++;
+				if (permutation[-d] != prev) break;
 			}
 		}
 		else {
@@ -127,15 +128,9 @@ void get_permutations_distances(int i_from, int i_to)
 			if (d < best_distance) {
 				best_distance = d;
 				best_permutation = permutation;
-				std::cout << std::hash<std::thread::id>{}(std::this_thread::get_id()) << " - ";
-				std::cout << best_distance << std::endl;
-				for (int i = 0; i < best_permutation.size(); i++)
-				{
-					std::cout << best_permutation.at(i) << " ";
-				}
-				std::cout << std::endl;
 			}
-			std::next_permutation(permutation.begin() + 1, permutation.end());
+			std::next_permutation(permutation.begin(), permutation.end());
+			i++;
 		}
 	}
 }
@@ -144,15 +139,13 @@ bool read_tsp_file(const char* fname)
 {
 	readMatricesFromFile(fname);
 
-	int number_of_permutations = factorial(number_of_items);
+	int number_of_permutations = factorial(number_of_items) - 1;
 		int worker_cnt = thread::hardware_concurrency();
-		int chunksize = std::ceil(static_cast<float>(number_of_permutations) / static_cast<float>(worker_cnt));
+		int chunksize = std::ceil(static_cast<float>(number_of_permutations) / static_cast<float>(worker_cnt)) - 1;
 		vector<thread> workers;
-
-
 		for (int id = 0; id < worker_cnt; ++id)
 		{
-			int i_from = id * chunksize;
+			int i_from = id * (chunksize - 1);
 			int i_to = (id + 1) * chunksize;
 			i_to = i_to < number_of_permutations ? i_to : number_of_permutations;
 			workers.push_back(thread(get_permutations_distances, i_from, i_to));
@@ -165,7 +158,7 @@ bool read_tsp_file(const char* fname)
 		cout << best_distance << endl;
 		for (int i = 0; i < best_permutation.size(); i++)
 		{
-			std::cout << best_permutation.at(i);
+			std::cout << best_permutation.at(i) << " ";
 		}
 		std::cout << std::endl;
 	return true;
@@ -173,6 +166,6 @@ bool read_tsp_file(const char* fname)
 
 int main()
 {
-	read_tsp_file("C:\\Users\\sofia\\Documents\\vsb\\7S\\pa\\cviceni\\2\\input.dat");
+	read_tsp_file("input.dat");
 	return 0;
 }
